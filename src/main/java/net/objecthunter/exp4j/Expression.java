e0667adff7a69e8831cbafd67c1b7228ed1c5950
Dmitriy Dumanskiy <doom369@gmail.com>
2019-01-02 14:21:28 +0200

    seprate function for token process

--- src/main/java/net/objecthunter/exp4j/Expression.java
+++ src/main/java/net/objecthunter/exp4j/Expression.java
@@ -165,55 +165,58 @@ public class Expression {
 
     public double evaluate() {
         final ArrayStack output = new ArrayStack();
-        for (Token t : tokens) {
-            int tokenType = t.getType();
-            switch (tokenType) {
-                case Token.TOKEN_NUMBER :
-                    output.push(((NumberToken) t).getValue());
-                    break;
-                case Token.TOKEN_VARIABLE :
-                    final String name = ((VariableToken) t).getName();
-                    final Double value = this.variables.get(name);
-                    if (value == null) {
-                        throw new IllegalArgumentException("No value has been set for the setVariable '" + name + "'.");
-                    }
-                    output.push(value);
-                    break;
-                case Token.TOKEN_OPERATOR :
-                    OperatorToken op = (OperatorToken) t;
-                    if (output.size() < op.getOperator().getNumOperands()) {
-                        throw new IllegalArgumentException("Invalid number of operands available for '" + op.getOperator().getSymbol() + "' operator");
-                    }
-                    if (op.getOperator().getNumOperands() == 2) {
-                        /* pop the operands and push the result of the operation */
-                        double rightArg = output.pop();
-                        double leftArg = output.pop();
-                        output.push(op.getOperator().apply(leftArg, rightArg));
-                    } else if (op.getOperator().getNumOperands() == 1) {
-                        /* pop the operand and push the result of the operation */
-                        double arg = output.pop();
-                        output.push(op.getOperator().apply(arg));
-                    }
-                    break;
-                case Token.TOKEN_FUNCTION :
-                    FunctionToken func = (FunctionToken) t;
-                    final int numArguments = func.getFunction().getNumArguments();
-                    if (output.size() < numArguments) {
-                        throw new IllegalArgumentException("Invalid number of arguments available for '" + func.getFunction().getName() + "' function");
-                    }
-                    /* collect the arguments from the stack */
-                    double[] args = new double[numArguments];
-                    for (int j = numArguments - 1; j >= 0; j--) {
-                        args[j] = output.pop();
-                    }
-                    output.push(func.getFunction().apply(args));
-                    break;
-
-            }
+        for (Token token : tokens) {
+            processToken(token, output);
         }
         if (output.size() > 1) {
             throw new IllegalArgumentException("Invalid number of items on the output queue. Might be caused by an invalid number of arguments for a function.");
         }
         return output.pop();
+    }
+
+    private void processToken(Token t, ArrayStack output) {
+        int tokenType = t.getType();
+        switch (tokenType) {
+            case Token.TOKEN_NUMBER :
+                output.push(((NumberToken) t).getValue());
+                break;
+            case Token.TOKEN_VARIABLE :
+                final String name = ((VariableToken) t).getName();
+                final Double value = this.variables.get(name);
+                if (value == null) {
+                    throw new IllegalArgumentException("No value has been set for the setVariable '" + name + "'.");
+                }
+                output.push(value);
+                break;
+            case Token.TOKEN_OPERATOR :
+                OperatorToken op = (OperatorToken) t;
+                if (output.size() < op.getOperator().getNumOperands()) {
+                    throw new IllegalArgumentException("Invalid number of operands available for '" + op.getOperator().getSymbol() + "' operator");
+                }
+                if (op.getOperator().getNumOperands() == 2) {
+                    /* pop the operands and push the result of the operation */
+                    double rightArg = output.pop();
+                    double leftArg = output.pop();
+                    output.push(op.getOperator().apply(leftArg, rightArg));
+                } else if (op.getOperator().getNumOperands() == 1) {
+                    /* pop the operand and push the result of the operation */
+                    double arg = output.pop();
+                    output.push(op.getOperator().apply(arg));
+                }
+                break;
+            case Token.TOKEN_FUNCTION :
+                FunctionToken func = (FunctionToken) t;
+                final int numArguments = func.getFunction().getNumArguments();
+                if (output.size() < numArguments) {
+                    throw new IllegalArgumentException("Invalid number of arguments available for '" + func.getFunction().getName() + "' function");
+                }
+                /* collect the arguments from the stack */
+                double[] args = new double[numArguments];
+                for (int j = numArguments - 1; j >= 0; j--) {
+                    args[j] = output.pop();
+                }
+                output.push(func.getFunction().apply(args));
+                break;
+        }
     }
 }
